6079c0e936dc2e6fb1895bc10370134ba4ca8ed3
adeshmalunjkar <adesh.malunjkar@upgrad.com>
2025-04-03 13:42:27 +0530

    Initial commit

---
+++ chapter-2/item2/builder/NutritionFacts.java
@@ -0,0 +1,75 @@
+
+
+public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public NutritionFacts(Builder builder) {
+
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+
+    public static Builder builder(int servingSize, int servings) {
+        return new Builder(servingSize, servings);
+    }
+
+    public static class Builder {
+        private final int servingSize;
+        private final int servings;
+        private int calories;
+        private int fat;
+        private int sodium;
+        private int carbohydrate;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        var nutrifatcs= NutritionFacts.builder(2,2)
+                .calories(200)
+                .fat(80)
+                .sodium(90)
+                .carbohydrate(10)
+                .build();
+        System.out.println(nutrifatcs);
+    }
+
+
+}
